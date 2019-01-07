5761289547a4f5eda15b1540275a9e56a8428441
Roman Proshin <roman@proshin.org>
2019-01-07 18:44:08 +0300

    #87: Configure auto deploy of artifacts to maven central repo (#89)

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/bash
+set -ev
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     mvn deploy -P sign,build-extras --settings mvnsettings.xml
 fi
