f6d0b0bc2a8bdd1b6d9f8971d48e931592297126
Roman Proshin <roman@proshin.org>
2019-01-07 18:43:11 +0300

    #87: Configure auto deploy of artifacts to maven central repo

--- before-deploy.sh
+++ before-deploy.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/bash
+set -ev
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_f06771379276_key -iv $encrypted_f06771379276_iv -in codesigning.asc.enc -out codesigning.asc -d
     gpg --fast-import cd/signingkey.asc
