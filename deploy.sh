5eb32fb20b79480708c2acdacd5026413891928b
Roman Proshin <roman@proshin.org>
2020-03-22 18:00:48 +0100

    #297: automate release

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 set -ev
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    mvn deploy -P sign,build-extras --settings mvnsettings.xml
+if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    if [ "$TRAVIS_BRANCH" == 'master' ]; then
+        echo "Run mvn deploy... for the master branch"
+        mvn deploy -P sign,build-extras --settings mvnsettings.xml
+    elif [[ $TRAVIS_BRANCH = "v"* ]]; then
+        echo "Run mvn deploy... for the release branch"
+        mvn deploy -P sign,build-extras --settings mvnsettings.xml
+    else
+        echo "The current branch is neither master nor v* - no deploy"
+    fi
+else
+    echo "The current branch looks like a pull request - no deploy"
 fi
