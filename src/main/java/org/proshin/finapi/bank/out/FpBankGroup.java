661c1bd2a0e2025b131b1f730520be0fa0245727
Constantin Costescu <constantineugen.costescu@gmail.com>
2020-07-24 00:59:48 +0200

    fix license

--- src/main/java/org/proshin/finapi/bank/out/FpBankGroup.java
+++ src/main/java/org/proshin/finapi/bank/out/FpBankGroup.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Roman Proshin
+ * Copyright 2020 Roman Proshin
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
